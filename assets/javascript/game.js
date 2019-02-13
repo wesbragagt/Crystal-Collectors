2c730f478c2ecf17e82cf717973dd94cec0865c6
Weslley Braga <wesbragagt@gmail.com>
2019-02-12 18:43:50 -0600

    initialized the primary functionality of the game, generate number and display

---
+++ assets/javascript/game.js
@@ -0,0 +1,35 @@
+$(document).ready(function () {
+    var number = Math.floor(Math.random() * 100) + 1;
+    console.log('computer picked ' + number);
+
+    $('#random-number').text(number);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
+
+
+
+// Generate Random Number and display it
+// A $( document ).ready() block.
+// Each Crystal takes a random value from an array
+// results panel displays total score
