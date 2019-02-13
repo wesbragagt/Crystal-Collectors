80e55d2a12dc49eb6e22d3313f23dc16764acb07
Weslley Braga <wesbragagt@gmail.com>
2019-02-13 15:39:41 -0600

    assigned a random generated value to each crystal variable

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,10 +1,32 @@
-$(document).ready(function () {
-    var number = Math.floor(Math.random() * 100) + 1;
-    console.log('computer picked ' + number);
+// A $( document ).ready() block.
 
+$(document).ready(function () {
+
+    // Function Generate Random Number 
+    var generateNumber = function (alpha) {
+        return Math.floor(Math.random() * alpha) + 1;
+    }
+    // assign to number and display it
+    var number = generateNumber(100);
+    console.log('computer picked ' + number);
     $('#random-number').text(number);
 
+    // Create an array for the crystal values
+    var crystalArray = [];
+    for (var i = 0; i < 4; i++) {
+        crystalArray.push(generateNumber(17))
+    }
+    console.log(crystalArray);
+    // Each Crystal gets assigned a value
+    var red = crystalArray[0]
+    var blue = crystalArray[1]
+    var purple = crystalArray[2]
+    var green = crystalArray[3]
 
+    console.log('red crystal value is ' + red);
+    console.log('blue crystal value is ' + blue);
+    console.log('purple crystal value is ' + purple);
+    console.log('green crystal value is ' + green);
 
 
 
@@ -29,7 +51,7 @@ $(document).ready(function () {
 
 
 
-// Generate Random Number and display it
-// A $( document ).ready() block.
-// Each Crystal takes a random value from an array
+
+
+
 // results panel displays total score
